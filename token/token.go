dec2d5cd3ad2793b310f65eabd8de6be11d46b88
mara <mara@the-witch.ru>
2023-05-20 17:53:04 +0400

    lexer

--- token/token.go
+++ token/token.go
@@ -8,23 +8,59 @@ type Token struct {
 }
 
 const (
-    illegal = "ILLEGAL"
-    EOF = "EOF"
-    
-    IDENT = "IDENT"
-    INT = "INT"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-    ASSIGN = "="
-    PLUS = "+"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
 
-    COMMA = ","
-    SEMICOLON = ";"
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-    LPAREN = "("
-    RPAREN = ")"
-    LBRACE = "{"
-    RBRACE = "}"
-    
-    FUNCTION = "FUNCTION"
-    let = "let"
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType {
+    "fn": FUNCTION,
+    "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
